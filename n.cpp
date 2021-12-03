7ce93aa875e1b5cd62e4cee378c0d6c79f791ace
Tzuyu Huang <fcebk17@gmail.com>
2021-12-03 22:38:16 +0800

    1st ver

---
+++ n.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<10;i++){
+        cout<<i<<endl;
+    }
+    cout<<"#"<<endl;
+    for(int i=0;i<10;++i){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
